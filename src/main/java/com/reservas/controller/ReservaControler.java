a17d1456be48912081dd3ee989993e2060a2da9b
pablo <pabloarmasbosse@gmail.com>
2025-11-01 17:06:53 +0100

    Implementada la lógica de la clase Reserva y la interfaz de la vista de reservas, que las muestra en una tabla.

--- src/main/java/com/reservas/controller/ReservaControler.java
+++ src/main/java/com/reservas/controller/ReservaControler.java
@@ -1,4 +1,60 @@
 package com.reservas.controller;
 
+import com.reservas.dao.ReservaDAO;
+import com.reservas.model.Reserva;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 public class ReservaControler {
+    ReservaDAO reservaDAO;
+    @FXML
+    TableView<Reserva> tableView;
+    @FXML
+    TableColumn<Reserva, Integer> colID;
+    @FXML
+    TableColumn<Reserva, Integer> colID_cliente;
+    @FXML
+    TableColumn<Reserva, Integer> colID_prop;
+    @FXML
+    TableColumn<Reserva, String> colFechaIni;
+    @FXML
+    TableColumn<Reserva, String> colFechaFin;
+    @FXML
+    TableColumn<Reserva, Integer> colNumPersonas;
+    @FXML
+    TableColumn<Reserva, Reserva.EstadoReserva> colEstado;
+    @FXML
+    TableColumn<Reserva, Double> colPrecio;
+    @FXML
+    TableColumn<Reserva, String> colMotivo;
+    ObservableList<Reserva> reservas;
+    @FXML
+    public void initialize(){
+        reservaDAO = new ReservaDAO();
+        reservas = FXCollections.observableArrayList(reservaDAO.getReservas());
+        configurarColumnas();
+        tableView.setItems(reservas);
+    }
+
+    private void configurarColumnas() {
+        colID.setCellValueFactory(new PropertyValueFactory<>("id_reserva"));
+        colID_cliente.setCellValueFactory(new PropertyValueFactory<>("id_cliente"));
+        colID_prop.setCellValueFactory(new PropertyValueFactory<>("id_propiedad"));
+        colFechaIni.setCellValueFactory(reservaDateCellDataFeatures ->
+                new SimpleStringProperty(reservaDateCellDataFeatures.getValue().getFechaIniString()));
+        colFechaFin.setCellValueFactory(reservaDateCellDataFeatures ->
+                new SimpleStringProperty(reservaDateCellDataFeatures.getValue().getFechaFinString()));
+        colNumPersonas.setCellValueFactory(new PropertyValueFactory<>("num_personas"));
+        colEstado.setCellValueFactory(new PropertyValueFactory<>("estadoReserva"));
+        colPrecio.setCellValueFactory(new PropertyValueFactory<>("precio_total"));
+        colMotivo.setCellValueFactory(new PropertyValueFactory<>("motivo_cancelacion"));
+    }
+    public void aniadirReserva(){
+
+    }
 }
--- src/main/java/com/reservas/dao/ReservaDAO.java
+++ src/main/java/com/reservas/dao/ReservaDAO.java
@@ -1,4 +1,45 @@
 package com.reservas.dao;
 
+import com.reservas.config.DataBaseConnection;
+import com.reservas.model.Reserva;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class ReservaDAO {
+    Connection connection;
+    ArrayList<Reserva> reservas;
+
+    public ReservaDAO() {
+        connection = DataBaseConnection.getInstance().conectarBD();
+        reservas = new ArrayList<>();
+    }
+    public ArrayList<Reserva> getReservas(){
+        String sql = "select id_reserva, id_cliente, id_propiedad, fecha_inicio," +
+                " fecha_fin, num_personas, estado, precio_total, motivo_cancelacion from reservas";
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+            ResultSet rs = statement.getResultSet();
+            while (rs.next()){
+                Reserva reserva = new Reserva();
+                reserva.setId_reserva(rs.getInt("id_reserva"));
+                reserva.setId_cliente(rs.getInt("id_cliente"));
+                reserva.setId_propiedad(rs.getInt("id_propiedad"));
+                reserva.setFecha_inicio(rs.getDate("fecha_inicio"));
+                reserva.setFecha_fin(rs.getDate("fecha_fin"));
+                reserva.setNum_personas(rs.getInt("num_personas"));
+                reserva.setEstado(Reserva.EstadoReserva.valueOf(rs.getString("estado").toUpperCase().trim()));
+                reserva.setPrecio_total(rs.getDouble("precio_total"));
+                reserva.setMotivo_cancelacion(rs.getString("motivo_cancelacion"));
+                reservas.add(reserva);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return reservas;
+    }
 }
